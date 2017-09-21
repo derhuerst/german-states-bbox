ca2ef04aa4e82e514b8f72e30d8ebbc567a13e78
Jannis R <mail@jannisr.de>
2017-09-21 14:05:37 +0200

    object format, 1.0.0

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const test = require('tape')
-const bbox = require('./index')
+const bboxes = require('./index')
 
 test('should be numbers', (t) => {
-	t.plan(4 * Object.keys(bbox).length)
-	for (let state in bbox) {
-		for (let value of bbox[state])
-			t.equal(typeof value, 'number', `${state} is invalid`)
+	t.plan(6 * Object.keys(bboxes).length)
+	for (let state in bboxes) {
+		const bbox = bboxes[state]
+
+		t.equal(typeof bbox.minLat, 'number', `minLat of ${state} is invalid`)
+		t.equal(typeof bbox.maxLat, 'number', `maxLat of ${state} is invalid`)
+		t.equal(typeof bbox.maxLon, 'number', `maxLon of ${state} is invalid`)
+		t.equal(typeof bbox.maxLon, 'number', `maxLon of ${state} is invalid`)
+
+		t.ok(bbox.minLat < bbox.maxLat, `${state} is invalid`)
+		t.ok(bbox.minLon < bbox.maxLon, `${state} is invalid`)
 	}
 })
